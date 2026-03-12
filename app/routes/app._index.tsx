92dc5881f12cdf42da89b3ff5eb043542f052ee6
marcelino <marcelino@adontechnologies.com>
2026-03-12 16:58:15 +0200

    init

---
+++ app/routes/app._index.tsx
@@ -0,0 +1,23 @@
+import type {
+  HeadersFunction,
+  LoaderFunctionArgs,
+} from "react-router";
+import { authenticate } from "../shopify.server";
+import { boundary } from "@shopify/shopify-app-react-router/server";
+
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+  await authenticate.admin(request);
+
+  return null;
+};
+
+export default function Index() {
+
+  return (
+    <div> hello </div>
+  );
+}
+
+export const headers: HeadersFunction = (headersArgs) => {
+  return boundary.headers(headersArgs);
+};
